bf0340bb6caf6c31f5d43a532ae04d402fa624d5
Steve Vance <vance.steven@gmail.com>
2026-08-20 23:51:28 -0700

    Expose legacy Mars parity column

--- planethrak/__init__.py
+++ planethrak/__init__.py
@@ -6,12 +6,15 @@ workflow before introducing more general fracture constitutive models.
 
 from .fronts import CrackingFront, load_front
 from .intersection import CrackingIntersection, find_cracking_intersection
+from .legacy_structure import RadialColumn, legacy_mars_column
 from .radiogenic import past_radiogenic_heat_uthk
 
 __all__ = [
     "CrackingFront",
     "CrackingIntersection",
+    "RadialColumn",
     "find_cracking_intersection",
+    "legacy_mars_column",
     "load_front",
     "past_radiogenic_heat_uthk",
 ]
