81e46df88783c1d22bd3da0e111b0d7c261310a9
Steve Vance <vance.steven@gmail.com>
2026-08-21 00:30:38 -0700

    Expose fracture column and alteration-capacity APIs

--- planethrak/__init__.py
+++ planethrak/__init__.py
@@ -4,6 +4,12 @@ The first development phase reproduces the archived MATLAB lookup-table
 workflow before introducing more general fracture constitutive models.
 """
 
+from .capacity import (
+    fractured_reactive_volume,
+    maximum_bound_water_mass,
+    spherical_shell_volumes,
+)
+from .column import FractureColumn, LegacyAccessibility, legacy_front_accessibility
 from .fronts import CrackingFront, load_front
 from .intersection import CrackingIntersection, find_cracking_intersection
 from .legacy_structure import RadialColumn, legacy_mars_column
@@ -12,9 +18,15 @@ from .radiogenic import past_radiogenic_heat_uthk
 __all__ = [
     "CrackingFront",
     "CrackingIntersection",
+    "FractureColumn",
+    "LegacyAccessibility",
     "RadialColumn",
     "find_cracking_intersection",
+    "fractured_reactive_volume",
+    "legacy_front_accessibility",
     "legacy_mars_column",
     "load_front",
+    "maximum_bound_water_mass",
     "past_radiogenic_heat_uthk",
+    "spherical_shell_volumes",
 ]
