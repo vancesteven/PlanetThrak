dd12ae769f84fcdb529afc7452883ed961d4f67e
Steve Vance <vance.steven@gmail.com>
2026-08-20 23:49:54 -0700

    Expose radiogenic heating in Python API

--- planethrak/__init__.py
+++ planethrak/__init__.py
@@ -6,10 +6,12 @@ workflow before introducing more general fracture constitutive models.
 
 from .fronts import CrackingFront, load_front
 from .intersection import CrackingIntersection, find_cracking_intersection
+from .radiogenic import past_radiogenic_heat_uthk
 
 __all__ = [
     "CrackingFront",
     "CrackingIntersection",
     "find_cracking_intersection",
     "load_front",
+    "past_radiogenic_heat_uthk",
 ]
